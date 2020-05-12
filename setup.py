96f4a041c20350ff596c99a9514493ad0b2c6ac7
Cosmin <cosmin.grozea@solarwinds.com>
2020-05-12 23:39:31 +0300

    Use setuptools-scm to publish new version on tag

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import
 
 import se_dns
-import distutils.core
+import setuptools
 
 REQUIRES = ["dnspython"]
 DESCRIPTION = """Simple dns tools."""
@@ -16,11 +16,18 @@ CLASSIFIERS = [
     "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
 ]
 
-distutils.core.setup(
+setuptools.setup(
     name='se-dns',
     description=DESCRIPTION,
     author="SpamExperts",
-    version=se_dns.__version__,
+    use_scm_version={
+        "write_to": "se_dns/__init__.py",
+        'write_to_template': '__version__ = "{version}"\n',
+        'tag_regex': r'^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$',
+        'version_scheme': lambda version: version.tag.public,
+        'local_scheme': lambda x: ""
+    },
+    setup_requires=['setuptools_scm'],
     license='GPL',
     platforms='POSIX',
     keywords='server',
